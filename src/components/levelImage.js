09a948de134a79f8d4d8e685ac23b9cec6d21945
Ville Jänkälä <ville.jankala@gmail.com>
2020-06-27 16:20:55 +0300

    Load inline svg

--- src/components/levelImage.js
+++ src/components/levelImage.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled, { css } from "styled-components";
+import axios from "axios";
 
 const Image = styled.div`
-  height: ${props => props.height};
+  height: ${(props) => props.height};
   > div {
-    ${props =>
+    ${(props) =>
       props.fullscreen &&
       css`
         top: 0;
@@ -16,19 +17,49 @@ const Image = styled.div`
     height: 100%;
     background: #333;
     border: 10px solid #333;
-    ${props =>
-      props.level &&
-      css`background-image: url("${
-        process.env.REACT_APP_API_URL
-      }/levels/${props => props.level}/map");`}
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center;
+  }
+
+  svg .sky {
+  }
+
+  svg .APPLE {
+  }
+
+  svg .KILLER {
+  }
+
+  svg .FLOWER {
+  }
+
+  svg .START {
+  }
+
+  svg {
+    overflow: hidden;
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
   }
 `;
 
 const LevelImage = ({ level, height }) => {
   const [fs, setFs] = useState(false);
+  const [svg, setSvg] = useState(null);
+
+  useEffect(() => {
+    const f = async () => {
+      try {
+        const { data } = await axios.get(
+          `${process.env.REACT_APP_API_URL}/levels/${level}/map`
+        );
+        setSvg(data);
+      } catch (e) {
+        console.log("error loading svg", e);
+      }
+    };
+    f();
+  }, [level]);
+
   return (
     <Image
       level={level}
@@ -38,7 +69,7 @@ const LevelImage = ({ level, height }) => {
         setFs(!fs);
       }}
     >
-      <div></div>
+      <div dangerouslySetInnerHTML={{ __html: svg }} />
     </Image>
   );
 };
